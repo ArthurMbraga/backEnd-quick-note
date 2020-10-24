126d1efed3957a5503497194b395a5ecb1ac4f12
Arthur Martins Braga <arthurmbraga1@gmail.com>
2020-10-23 22:44:26 -0300

    criando as migrations

---
+++ src/database/migrations/20201023222245_user.js
@@ -0,0 +1,11 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("user", function (table) {
+    table.string("user_id").primary().notNullable();
+    table.string("name").notNullable();
+    table.string("email").notNullable();
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("user");
+};
