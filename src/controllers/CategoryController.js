c311cc287c0fd209e246929b98ce7f431c38d772
Arthur Martins Braga <arthurmbraga1@gmail.com>
2020-11-11 01:21:59 -0300

    Validators

--- src/controllers/CategoryController.js
+++ src/controllers/CategoryController.js
@@ -57,4 +57,62 @@ module.exports = {
       });
     }
   },
+
+  async getByUser(user_id, { categoryName }) {
+    const filter = { "note.user_id": user_id };
+
+    if (categoryName) filter["category.name"] = categoryName;
+
+    let notes = await connection("note")
+      .join("note_category", "note_category.note_id", "note.note_id")
+      .join("category", "note_category.category_id", "category.category_id")
+      .where(filter)
+      .select("note.*");
+
+    const notes_id = notes.map((note) => note.note_id);
+    notes = _.keyBy(notes, "note_id");
+
+    let categories = await connection("note_category")
+      .join("category", "note_category.category_id", "category.category_id")
+      .whereIn("note_category.note_id", notes_id)
+      .select("note_category.note_id", "category.category_id", "category.name");
+
+    categories = _.groupBy(categories, "note_id");
+
+    Object.keys(categories).forEach((note_id) => {
+      const newCategories = categories[note_id];
+      newCategories.map((category) => {
+        delete category.note_id;
+        return category;
+      });
+
+      categories[note_id] = newCategories;
+    });
+
+    const result = Object.keys(notes).map((note_id) => {
+      const note = notes[note_id];
+      note.categories = categories[note_id];
+
+      return note;
+    });
+
+    return result;
+  },
+  
+  async getByUser(request, response) {
+    try {
+      const { user_id } = request.params;
+      const result = await NoteModel.getByUser(user_id, request.query);
+
+      return response.status(200).json(result);
+    } catch (error) {
+      console.warn("Note get failed:", error);
+
+      return response.status(500).json({
+        notification: "Internal server error while trying to get Note",
+      });
+    }
+  },
 };
+
+
--- src/validators/NoteValidator.js
+++ src/validators/NoteValidator.js
@@ -1,32 +1,33 @@
 const { celebrate, Segments, Joi } = require("celebrate");
 
 module.exports = {
-  
   create: celebrate({
     [Segments.BODY]: Joi.object().keys({
-      user_id: Joi.string(),
-      title: Joi.string(),
-      description: Joi.string(),
+      user_id: Joi.string().required(),
+      title: Joi.string().required(),
+      description: Joi.string().required(),
     }),
   }),
-
-  getById: celebrate({
+  getByUser: celebrate({
     [Segments.PARAMS]: Joi.object().keys({
-      note_id: Joi.string().required(),
+      user_id: Joi.string().required(),
+    }),
+    [Segments.QUERY]: Joi.object().keys({
+      categoryName: Joi.string().optional(),
     }),
   }),
-
   update: celebrate({
     [Segments.PARAMS]: Joi.object().keys({
       note_id: Joi.string().required(),
     }),
-    [Segments.BODY]: Joi.object().keys({
-      user_id: Joi.string().optional(),
-      title: Joi.string().optional(),
-      description: Joi.string().optional(),
-    }),    
+    [Segments.BODY]: Joi.object()
+      .keys({
+        note_id: Joi.string().optional(),
+        title: Joi.string().optional(),
+        description: Joi.string().optional(),
+      })
+      .min(1),
   }),
-  
   delete: celebrate({
     [Segments.PARAMS]: Joi.object().keys({
       note_id: Joi.string().required(),
